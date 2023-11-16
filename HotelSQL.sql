9ad611eed6c4f8260bd9e6839c213c57d0e744cd
Yordan Yordanov <126782519+dancho393@users.noreply.github.com>
2023-11-17 01:59:53 +0200

    Update HotelSQL.sql
    added trigger to calculate the price for the reservation based on how many rooms are reserved
    
    --To Do -- to add services to total price of reservation too

--- HotelSQL.sql
+++ HotelSQL.sql
@@ -36,6 +36,49 @@ CREATE TABLE IF NOT EXISTS public.reservations(
     FOREIGN KEY (guest_id) REFERENCES public.guests(id)
 );
 
+
+
+
+-- TRIGGER FUNCTION
+CREATE OR REPLACE FUNCTION calculate_reservation_price_with_services()
+RETURNS TRIGGER AS $$
+DECLARE
+  room_price NUMERIC(6,2);
+  service_price NUMERIC(6,2);
+BEGIN
+  -- Calculate room price
+  room_price = (
+    SELECT SUM(r.price_per_night * (res.date_to - res.date_from + 1))
+    FROM rooms r
+    JOIN rooms_reservations rr ON r.id = rr.room_id
+    JOIN reservations res ON rr.reservation_id = res.id
+    WHERE res.id = NEW.reservation_id
+  );
+
+  -- Calculate service price
+  service_price = COALESCE((
+    SELECT SUM(s.price * s.quantity)
+    FROM services s
+    JOIN services_reservations sr ON s.id = sr.service_id
+    WHERE sr.reservation_id = NEW.reservation_id
+  ), 0.0);
+
+  -- Update total_price in reservations table
+  UPDATE reservations
+  SET total_price = room_price + service_price
+  WHERE id = NEW.reservation_id;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- TRIGGER
+CREATE TRIGGER calculate_reservation_price_with_services_trigger
+AFTER INSERT OR UPDATE ON public.rooms_reservations
+FOR EACH ROW
+EXECUTE FUNCTION calculate_reservation_price_with_services();
+
+
 CREATE TABLE IF NOT EXISTS public.services(
     id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(100) NOT NULL,
