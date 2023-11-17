3156916b5eda9685e55aae82ed5ba75a6ceedc0c
Nikola Gerdzhikov <101010513+NikolaGer18@users.noreply.github.com>
2023-11-17 02:07:13 +0200

    Update HotelSQL.sql
    Updated tables

--- HotelSQL.sql
+++ HotelSQL.sql
@@ -17,7 +17,10 @@ CREATE TABLE IF NOT EXISTS public.rooms (
     id SERIAL PRIMARY KEY NOT NULL,
     name VARCHAR(100) NOT NULL,
     price_per_night NUMERIC(6,2) NOT NULL,
-    capacity INTEGER NOT NULL
+    capacity INTEGER NOT NULL,
+	room_type_id INT NOT NULL,
+	FOREIGN KEY (room_type_id) REFERENCES public.room_types(id)
+	
 );
 
 CREATE TABLE IF NOT EXISTS public.payment_types(
@@ -35,7 +38,6 @@ CREATE TABLE IF NOT EXISTS public.reservations(
     FOREIGN KEY (payment_type_id) REFERENCES public.payment_types (id),
     FOREIGN KEY (guest_id) REFERENCES public.guests(id)
 );
-
 
 
 
@@ -78,22 +80,3 @@ AFTER INSERT OR UPDATE ON public.rooms_reservations
 FOR EACH ROW
 EXECUTE FUNCTION calculate_reservation_price_with_services();
 
-
-CREATE TABLE IF NOT EXISTS public.services(
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    price NUMERIC(6,2),
-    quantity INTEGER NOT NULL
-);
-CREATE TABLE IF NOT EXISTS public.services_reservations(
-    service_id INT NOT NULL,
-    reservation_id INT NOT NULL,
-    FOREIGN KEY (service_id) REFERENCES public.services (id),
-    FOREIGN KEY (reservation_id) REFERENCES public.reservations (id)
-);
-CREATE TABLE IF NOT EXISTS public.rooms_reservations(
-    room_id INT NOT NULL,
-    reservation_id INT NOT NULL,
-    FOREIGN KEY (room_id) REFERENCES public.rooms (id),
-    FOREIGN KEY (reservation_id) REFERENCES public.reservations (id)
-);
