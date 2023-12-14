b215019c625a0b20960c4516744706eccdb9f9d5
Yordan Yordanov <126782519+dancho393@users.noreply.github.com>
2023-12-14 12:51:59 +0200

    Update HotelSQL.sql
    final commit

--- HotelSQL.sql
+++ HotelSQL.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS public.payment_types(
 CREATE TABLE IF NOT EXISTS public.reservations (
     id SERIAL PRIMARY KEY NOT NULL,
     payment_type_id INT NOT NULL,
-	cancelled BOOLEAN NOT NULL,
+	cancelled BOOLEAN DEFAULT false NOT NULL,
     guest_id INT NOT NULL,
     date_from DATE NOT NULL,
     date_to DATE NOT NULL,
@@ -86,127 +86,7 @@ CREATE TABLE IF NOT EXISTS public.review(
 	FOREIGN KEY (guest_id) REFERENCES public.guests (id)
 );
 
-INSERT INTO public.review(room_id,guest_id,rating,coment) VALUES
-(1,NULL,5,'Primeren comentar'),
-(1,NULL,5,'Primeren comentar'),
-(1,NULL,3,'Primeren comentar'),
-(1,NULL,3,'Primeren comentar');
---INSERTS
-INSERT INTO public.payment_types (name) VALUES
-  ('Credit Card'),
-  ('Cash'),
-  ('PayPal'),
-  ('Bank Transfer'),
-  ('Venmo'),
-  ('Apple Pay'),
-  ('Google Pay'),
-  ('Bitcoin'),
-  ('Check'),
-  ('Amazon Pay');
-  
-  
-  
-    INSERT INTO public.seasonal_promotions(name, date_from, date_to)
-VALUES 
-    ('Year-Round Special', '2023-01-01', '2023-12-31'),
-    ('Spring Special Offer', '2023-03-20', '2023-06-20'),
-    ('Summer Special Offer', '2023-06-21', '2023-09-22'),
-    ('Autumn Special Offer', '2023-09-23', '2023-12-20'),
-    ('Winter Special Offer', '2023-09-21', '2024-03-19');
-	
-INSERT INTO public.room_types (name)
-VALUES 
-  ('Single Room'),
-  ('Double Room'),
-  ('Suite'),
-  ('Twin Room'),
-  ('Family Room');
 
-INSERT INTO public.service_names(name,price,season_id) 
-VALUES
-	('OutsidePool',15,3),
-	('InsidePool',25,1),
-	('Dinner',5,1),
-	('Lunch',5,1),
-	('Breakfast',5,1),
-	('SPA',10,1),
-	('Fitness',5,1);
-	
-	
-INSERT INTO public.rooms (name,price_per_night,capacity,room_type_id) VALUES
-  ('Room 101', 30, 1,1),
-  ('Room 102', 30, 1,1),
-  ('Room 103', 30, 4,5),
-  ('Room 104', 30, 1,1),
-  ('Room 201', 40, 2,2),
-  ('Room 202', 40, 2,2),
-  ('Room 203', 40, 2,2),
-  ('Room 204', 40, 2,2),
-  ('Room 204', 45, 5,3),
-  ('Room 301', 45, 3,3),
-  ('Room 302', 45, 4,5),
-  ('Room 303', 45, 2,2),
-  ('Room 304', 50, 4,4),
-  ('Room 401', 45, 5,3),
-  ('Room 402', 50, 2,4),
-  ('Room 403', 50, 4,4),
-  ('Room 404', 50, 4,4);
-  
-  INSERT INTO public.guests(name,info,email,phone_number,egn,birth_date)  VALUES
- ('Nikola','Student in TU VARNA','nikola123@abv.bg','+359883928323','0149283721','2001-08-15'),
-('Yordan','Teacher in TU VARNA','yordanY@abv.bg','+359892381323','9587362518','1995-06-19'),
-('Yancho','Student in MU VARNA','jane123@gmail.com','+359873285594','0142938295','2001-07-22'),
-('Aleksandar','Teacher in MU VARNA','alexP@gmail.com','+359887236217','9023920149','1990-01-23');
-  
-INSERT INTO public.reservations(payment_type_id, guest_id, date_from, date_to) VALUES
- (1, 1, '2023-11-01', '2023-11-05'),
-  (2, 2, '2023-11-10', '2023-11-15'),
-  (3, 3, '2023-11-05', '2023-11-08'),
-  (1, 4, '2023-11-20', '2023-11-25'),
-  (4, 3, '2023-11-15', '2023-11-20'),
-  (2, 3, '2023-11-03', '2023-11-07'),
-  (1, 3, '2023-11-08', '2023-11-12'),
-  (3, 3, '2023-11-02', '2023-11-06'),
-  (3, 3, '2023-11-12', '2023-11-17'),
-  (3, 3, '2023-11-22', '2023-11-27');
-  
-  
-  INSERT INTO public.reservations(payment_type_id, guest_id, date_from, date_to) VALUES
-  (1,1,'2023-12-13','2023-12-15');
-  
-
-INSERT INTO rooms_reservations (room_id, reservation_id)
-VALUES (11, 1); 
-  
- 
-  
-
-INSERT INTO rooms_reservations (room_id, reservation_id)
-VALUES (2, 1);
-
-INSERT INTO rooms_reservations (room_id, reservation_id)
-VALUES (3, 1);
-
-INSERT INTO rooms_reservations (room_id, reservation_id)
-VALUES (4, 1);
-
-INSERT INTO rooms_reservations (room_id, reservation_id)
-VALUES (5, 1);
-
-
-INSERT INTO public.services (service_name_id, quantity,reservation_id)
-VALUES
-  (29,4,1),
-  (30,4,1),
-  (31,4,1),
-  (32,4,1);
- 
-
-
-
---SELECT QUERIES
---  SELECT reservations(id),rooms(name),guests(name),reservation(date_from),reservation(date_to)
---  FROM public.reservations;
 
 
 
@@ -346,6 +226,133 @@ $$ LANGUAGE plpgsql;
 --5.0
 
 
+--INSERTS
+
+
+
+INSERT INTO public.payment_types (name) VALUES
+  ('Credit Card'),
+  ('Cash'),
+  ('PayPal'),
+  ('Bank Transfer'),
+  ('Venmo'),
+  ('Apple Pay'),
+  ('Google Pay'),
+  ('Bitcoin'),
+  ('Check'),
+  ('Amazon Pay');
+  
+  
+  
+    INSERT INTO public.seasonal_promotions(name, date_from, date_to)
+VALUES 
+    ('Year-Round Special', '2023-01-01', '2023-12-31'),
+    ('Spring Special Offer', '2023-03-20', '2023-06-20'),
+    ('Summer Special Offer', '2023-06-21', '2023-09-22'),
+    ('Autumn Special Offer', '2023-09-23', '2023-12-20'),
+    ('Winter Special Offer', '2023-09-21', '2024-03-19');
+	
+INSERT INTO public.room_types (name)
+VALUES 
+  ('Single Room'),
+  ('Double Room'),
+  ('Suite'),
+  ('Twin Room'),
+  ('Family Room');
+
+INSERT INTO public.service_names(name,price,season_id) 
+VALUES
+	('OutsidePool',15,3),
+	('InsidePool',25,1),
+	('Dinner',5,1),
+	('Lunch',5,1),
+	('Breakfast',5,1),
+	('SPA',10,1),
+	('Fitness',5,1);
+	
+	
+INSERT INTO public.rooms (name,price_per_night,capacity,room_type_id) VALUES
+  ('Room 101', 30, 1,1),
+  ('Room 102', 30, 1,1),
+  ('Room 103', 30, 4,5),
+  ('Room 104', 30, 1,1),
+  ('Room 201', 40, 2,2),
+  ('Room 202', 40, 2,2),
+  ('Room 203', 40, 2,2),
+  ('Room 204', 40, 2,2),
+  ('Room 204', 45, 5,3),
+  ('Room 301', 45, 3,3),
+  ('Room 302', 45, 4,5),
+  ('Room 303', 45, 2,2),
+  ('Room 304', 50, 4,4),
+  ('Room 401', 45, 5,3),
+  ('Room 402', 50, 2,4),
+  ('Room 403', 50, 4,4),
+  ('Room 404', 50, 4,4);
+  
+  INSERT INTO public.guests(name,info,email,phone_number,egn,birth_date)  VALUES
+ ('Nikola','Student in TU VARNA','nikola123@abv.bg','+359883928323','0149283721','2001-08-15'),
+('Yordan','Teacher in TU VARNA','yordanY@abv.bg','+359892381323','9587362518','1995-06-19'),
+('Yancho','Student in MU VARNA','jane123@gmail.com','+359873285594','0142938295','2001-07-22'),
+('Aleksandar','Teacher in MU VARNA','alexP@gmail.com','+359887236217','9023920149','1990-01-23');
+  
+INSERT INTO public.reservations(payment_type_id, guest_id, date_from, date_to,number_guests) VALUES
+ (1, 1, '2023-11-01', '2023-11-05',2),
+  (2, 2, '2023-11-10', '2023-11-15',2),
+  (3, 3, '2023-11-05', '2023-11-08',2)
+  (1, 4, '2023-11-20', '2023-11-25',2),
+  (4, 3, '2023-11-15', '2023-11-20',2),
+  (2, 3, '2023-11-03', '2023-11-07',2),
+  (1, 3, '2023-11-08', '2023-11-12',2),
+  (3, 3, '2023-11-02', '2023-11-06',2),
+  (3, 3, '2023-11-12', '2023-11-17',2),
+  (3, 3, '2023-11-22', '2023-11-27',2);
+  
+  
+  INSERT INTO public.reservations(payment_type_id, guest_id, date_from, date_to) VALUES
+  (1,1,'2023-12-13','2023-12-15',2);
+  
+
+INSERT INTO rooms_reservations (room_id, reservation_id)
+VALUES (11, 1); 
+  
+ 
+  
+
+INSERT INTO rooms_reservations (room_id, reservation_id)
+VALUES (2, 1);
+
+INSERT INTO rooms_reservations (room_id, reservation_id)
+VALUES (3, 1);
+
+INSERT INTO rooms_reservations (room_id, reservation_id)
+VALUES (4, 1);
+
+INSERT INTO rooms_reservations (room_id, reservation_id)
+VALUES (5, 1);
+
+
+INSERT INTO public.services (service_name_id, quantity,reservation_id)
+VALUES
+  (2,4,1),
+  (3,4,1),
+  (4,4,1),
+  (5,4,1);
+  
+  INSERT INTO public.review(room_id,guest_id,rating,coment) VALUES
+(1,NULL,5,'Primeren comentar'),
+(1,NULL,5,'Primeren comentar'),
+(1,NULL,3,'Primeren comentar'),
+(1,NULL,3,'Primeren comentar');
+ 
+
+
+
+
+
+
+
+
 ---SELECTSSSS
 --Да се направи заявка за отчет на пристигащите резервации към днешна дата 
 --(от гледна точка на рецепционист). Резултатът от заявката да съдържа 
@@ -419,4 +426,3 @@ LIMIT 5;
 UPDATE public.reservations SET cancelled = TRUE WHERE id = 31;
   
   
-
